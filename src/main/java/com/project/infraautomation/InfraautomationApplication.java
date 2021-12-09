d0125e98dd9924197c6a2d75c5d4b4e3467b5fd3
abij85 <anilbijalwan91@gmail.com>
2021-12-09 19:56:12 +0530

    base initial build

---
+++ src/main/java/com/project/infraautomation/InfraautomationApplication.java
@@ -0,0 +1,13 @@
+package com.project.infraautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/infraautomation/InfraautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.infraautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
