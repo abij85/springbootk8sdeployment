d9f87a767e45887c0cfb44150263d99bb512bc7e
abij85 <anilbijalwan91@gmail.com>
2021-12-09 21:19:00 +0530

    db deployment using liquibase full changes

--- src/main/java/com/project/infraautomation/DeployDatabaseApplication.java
+++ src/main/java/com/project/infraautomation/DeployDatabaseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfraautomationApplication {
+public class DeployDatabaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfraautomationApplication.class, args);
+		SpringApplication.run(DeployDatabaseApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/project/infraautomation/oraclesslconfig/OracleSsslConfig.java
@@ -0,0 +1,26 @@
+package com.project.infraautomation.oraclesslconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+class OracleSslConfig {
+    @Bean
+    public void oracleSslConfig() {
+
+        //VARS injected through kubernetes secrets
+        File truststore = new File(System.getenv("TRUSTSTORE_ENV_VAR"));
+        String tsp = truststore.getAbsolutePath();
+        System.setProperty("javax.net.ssl.trustStore", tsp);
+        System.setProperty("javax.net.ssl.trustStorePassword", System.getenv("PWD"));
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
+        File keystore = new File(System.getenv("KEYSTORE_ENV_VAR"));
+        String ksp = truststore.getAbsolutePath();
+        System.setProperty("javax.net.ssl.keyStore", ksp);
+        System.setProperty("javax.net.ssl.keyStorePassword", System.getenv("KEY_PWD"));
+        System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
+    }
+}
